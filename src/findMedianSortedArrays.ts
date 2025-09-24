c98ce95947a8083bcbc9ee641006f45361b1cbc3
Huynh Eric <huynheric@Huynhs-MacBook-Air.local>
2025-09-24 22:07:26 +0700

    Update: ...

---
+++ src/findMedianSortedArrays.ts
@@ -0,0 +1,16 @@
+/*
+ * [1, 3, 5, 7, 8]
+ * [2, 4, 6]
+ * [1, 2, 3, 4, 5, 6, 7, 8]
+*/
+
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+  const rs: number[] = [];
+  let p1 = 0;
+  let p2 = 0;
+  while (p1 < nums1.length && p2 < nums2.length) {
+  }
+  return 0;
+};
+
+console.log(findMedianSortedArrays([1, 3, 4, 9], [5, 7, 8]));
